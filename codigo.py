c1e9a37d2407829610f32c7f4e5a42909e169993
EvandroLuizdeOliveira <evandro.lu.oli@gmail.com>
2023-07-14 15:56:06 -0300

    Update 1

--- codigo.py
+++ codigo.py
@@ -17,21 +17,42 @@ while True:
     opcao = input(menu)
 
     if opcao == "1":
-        deposito = float(input("Digite o valor a ser depositado: "))
+        deposito = float(input("Digite o valor a ser depositado: R$"))
         if deposito > 0:
             saldo += deposito
-            extrato += f"{str(deposito:.2F)}\n"
+            extrato = f"Depósito R${deposito:.2F}\n{extrato}"
+            print("\nDepósito realizado com sucesso!")
         else:
-            print("Valor inválido!")
+            print("\nValor inválido!")
 
     elif opcao == "2":
-        print("Saque")
+        saque = float(input("Digite o valor a ser sacado: R$"))
+        if numero_saque == LIMITE_SAQUES:
+            print("\nLimite de saque diário atingido!")
+        elif saque > saldo:
+            print("\nSaldo insuficiente!")
+        elif saque > limite:
+            print("\nValor supera o limite de saque diário de R$500!")
+        elif saque <= limite and numero_saque <=3 and saldo >= saque:
+            saldo -= saque
+            numero_saque += 1
+            extrato = f"Saque R${saque:.2F}\n{extrato}"
+            print("\nSaque realizado com sucesso!")
+        else:
+            print("\nErro desconhecido!")
 
     elif opcao == "3":
-        print(extrato)
+        print()
+        print("Extrato".center(40, "#"))
+        if not extrato:
+            print("\nNão foram realizadas movimentações.")
+        elif extrato:
+            print(f"{extrato}",end="")
+        print(f"\nSaldo: R${saldo:.2F}")
+        print(f"{40 * '#'}")
     
     elif opcao == "4":
         break
 
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("\nOperação inválida, por favor selecione novamente a operação desejada.")
